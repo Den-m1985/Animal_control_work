5f69a5c9a871787fb40a1890941a70132760c65a
den-m1985 <denismartyanov244@gmail.com>
2023-07-07 14:53:23 +0200

    make some changes

--- src/main/java/org/example/animal/Pet.java
+++ src/main/java/org/example/animal/Pet.java
@@ -1,48 +1,10 @@
 package org.example.animal;
 
-import java.time.LocalDate;
 
-public class Pet extends Animal{
-    private static int idCounter = 0;
-    protected int id;
-    protected int number;
-    protected LocalDate dateOfBirth;
-    protected String name;
-    // commands: PetCommands
+public abstract class Pet extends Animal{
 
 
     public Pet() {
-        this.id = idCounter++;
     }
 
-    @Override
-    public LocalDate getDateOfBirth() {
-        return null;
-    }
-
-    @Override
-    public void setDateOfBirth(int year, int month, int day) {
-
-    }
-
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public void setName(String name) {
-
-    }
-
-    @Override
-    public int getId() {
-        return id;
-    }
-
-
-    @Override
-    public String getType() {
-        return null;
-    }
 }
---
+++ src/main/java/org/example/command/PetCommands.java
@@ -0,0 +1,17 @@
+package org.example.command;
+
+
+import java.util.ArrayList;
+
+public class PetCommands {
+
+    private ArrayList<Command> commands;
+
+
+    public void PetCommands(){
+    }
+
+    public void addCommand(Command command){
+    }
+
+}
